113561071e68d60ccf61e326784d47cc40a44862
Ryan McEvoy <rynosapien@hotmail.co.uk>
2024-05-08 22:57:43 +0100

    refactor: playing around with the XP gui

--- LethalProgression/Patches/HUDManager/HudManagerPatch.cs
+++ LethalProgression/Patches/HUDManager/HudManagerPatch.cs
@@ -1,4 +1,6 @@
 ﻿using HarmonyLib;
+using Mono.Cecil.Cil;
+using System;
 using System.Collections;
 using TMPro;
 using UnityEngine;
@@ -10,6 +12,7 @@ namespace LethalProgression.Patches;
 internal class HUDManagerPatch
 {
     private static GameObject _tempBar;
+    private static GameObject _bottomMiddle;
     private static TextMeshProUGUI _tempText;
     private static float _tempBarTime;
 
@@ -33,8 +36,14 @@ internal class HUDManagerPatch
         // Now we got the loot list that's about to be displayed, we add XP for each one that gets shown.
         int scrapCost = GObject.scrapValue;
 
+        float mult = LP_NetworkManager.xpInstance.skillList.skills[Skills.UpgradeType.Value].GetMultiplier();
+        float value = LP_NetworkManager.xpInstance.teamLootLevel.Value * mult;
+        float valueMultiplier = 1 + (value / 100f);
+
+        int realScrapCost = (int) Math.Floor(scrapCost / valueMultiplier);
+
         // Give XP for the amount of money this scrap costs.
-        LP_NetworkManager.xpInstance.updateTeamXPClientMessage.SendServer(scrapCost);
+        LP_NetworkManager.xpInstance.updateTeamXPClientMessage.SendServer(realScrapCost);
     }
 
     public static void ShowXPUpdate()
@@ -45,7 +54,7 @@ internal class HUDManagerPatch
         
         LC_XP xpInstance = LP_NetworkManager.xpInstance;
 
-        GameObject _tempprogress = GameObject.Find("/Systems/UI/Canvas/IngamePlayerHUD/BottomMiddle/XPUpdate/XPBarProgress");
+        GameObject _tempprogress = GameObject.Find("/Systems/UI/Canvas/IngamePlayerHUD/BottomMiddleXPStatus/XPUpdate/XPBarProgress");
 
         _tempprogress.GetComponent<Image>().fillAmount = xpInstance.teamXP.Value / (float)xpInstance.CalculateXPRequirement();
         _tempText.text = xpInstance.teamXP.Value + " / " + (float)xpInstance.CalculateXPRequirement();
@@ -105,27 +114,42 @@ internal class HUDManagerPatch
     {
         LethalPlugin.XPBarGUI.CreateAllObjectsIfRequired();
 
+        GameObject _refPlayerHUD = GameObject.Find("/Systems/UI/Canvas/IngamePlayerHUD");
+        GameObject _refBottomMiddle = GameObject.Find("/Systems/UI/Canvas/IngamePlayerHUD/BottomMiddle");
+
+        _bottomMiddle = GameObject.Instantiate(_refBottomMiddle);
+        _bottomMiddle.transform.SetParent(_refPlayerHUD.transform, false);
+        _bottomMiddle.name = "BottomMiddleXPStatus";
+
+        foreach(Transform child in _bottomMiddle.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+
         GameObject _xpBar = GameObject.Find("/Systems/UI/Canvas/QuickMenu/XpInfoContainer/XPBar");
         _tempBar = GameObject.Instantiate(_xpBar);
         _tempBar.name = "XPUpdate";
 
+        _tempBar.SetActive(false);
+
         _tempText = _tempBar.GetComponentInChildren<TextMeshProUGUI>();
 
-        GameObject _igHud = GameObject.Find("/Systems/UI/Canvas/IngamePlayerHUD/BottomMiddle");
-        _tempBar.transform.SetParent(_igHud.transform, false);
+        _tempBar.transform.SetParent(_bottomMiddle.transform, false);
         _tempBar.transform.localScale = new Vector3(0.4f, 0.4f, 0.4f);
 
-        GameObject _xpBarLevel = GameObject.Find("/Systems/UI/Canvas/IngamePlayerHUD/BottomMiddle/XPUpdate/XPLevel");
+        GameObject _xpBarLevel = GameObject.Find("/Systems/UI/Canvas/IngamePlayerHUD/BottomMiddleXPStatus/XPUpdate/XPLevel");
         GameObject.Destroy(_xpBarLevel);
-
-        GameObject _xpBarProfit = GameObject.Find("/Systems/UI/Canvas/IngamePlayerHUD/BottomMiddle/XPUpdate/XPProfit");
+        
+        GameObject _xpBarProfit = GameObject.Find("/Systems/UI/Canvas/IngamePlayerHUD/BottomMiddleXPStatus/XPUpdate/XPProfit");
         GameObject.Destroy(_xpBarProfit);
 
-        _tempBar.transform.Translate(3.1f, -2.1f, 0f);
-        Vector3 localPos = _tempBar.transform.localPosition;
+        Vector3 localPos = _bottomMiddle.transform.localPosition;
+        float containerSize = _bottomMiddle.GetComponent<RectTransform>().sizeDelta.x;
+        float barSize = _tempBar.GetComponent<RectTransform>().sizeDelta.x;
 
-        _tempBar.transform.localPosition = new Vector3(localPos.x, localPos.y + 5f, localPos.z);
+        LethalPlugin.Log.LogInfo($"Move Bar {containerSize} {barSize}");
 
-        _tempBar.SetActive(false);
+        _tempBar.transform.Translate(0f, 0f, 0f);
+        _tempBar.transform.localPosition = new Vector3(containerSize + 50f, localPos.y - 30f, localPos.z);
     }
 }
