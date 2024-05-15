73db4711f779eeaf61641fae81c8934d4a3b674c
Ryan McEvoy <rynosapien@hotmail.co.uk>
2024-05-15 23:38:38 +0100

    chore: concept

---
+++ LethalProgression/Config/GeneralConfig.cs
@@ -0,0 +1,12 @@
+// using LethalProgression.Config.Attributes;
+
+// namespace LethalProgression.Config;
+
+// [ConfigSection("General")]
+// interface IGeneralConfig {
+//     [ConfigName("XP Maximum")]
+//     [ConfigDescription("Maximum XP to level up.")]
+//     [ConfigDefault(750)]
+//     int maxXPCost;
+// }
+
